90be7315c74d63631d61d5466f9c3212d8cee352
Rahul Singhal <rahul.singhal@harness.io>
2026-04-14 18:37:18 +0530

    adding right image tag for x86

--- build-amd64.sh
+++ build-amd64.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Build image; use linux/amd64 (x86_64) when Docker buildx is available.
 set -e
-TAG="${1:-0.0.22}"
+TAG="${1:-0.0.38}"
 IMAGE="rahulsinghalharness/sourceairbytegithub"
 
 if docker buildx version &>/dev/null; then
